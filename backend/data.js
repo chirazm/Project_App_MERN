4cb753883d09eb2baf6d8f525f89e16b9cfa7107
chirazm <chirazmeddeb2@gmail.com>
2022-04-23 22:53:00 +0100

    second

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products : [
+        {
+            _id: '1',
+            name : 'Nike Slim Shirt',
+            category : 'Shirts',
+            image : '/images/p1.jpg',
+            price : 120,
+            countInStock : 10,
+            brand : 'Nike',
+            rating: 4.5,
+            numReviews : 10,
+            description : 'high quality product',
+        },
+        {
+            _id: '2',
+            name : 'Lacoste Slim Shirt',
+            category : 'Pants',
+            image : '/images/p2.jpg',
+            price : 120,
+            countInStock : 20,
+            brand : 'Nike',
+            rating: 4.5,
+            numReviews : 10,
+            description : 'high quality product',
+        },
+        {
+            _id: '3',
+            name : 'Nike Slim Shirt',
+            category : 'Shirts',
+            image : '/images/p3.jpg',
+            price : 120,
+            countInStock : 0,
+            brand : 'Nike',
+            rating: 4.5,
+            numReviews : 14,
+            description : 'high quality product',
+        },
+        {
+            _id: '4',
+            name : 'Adidas Slim Shirt',
+            category : 'Shirts',
+            image : '/images/p4.jpg',
+            price : 100,
+            countInStock : 12,
+            brand : 'Adidas',
+            rating: 4.5,
+            numReviews : 10,
+            description : 'high quality product',
+        },
+    ]
+}
+export default data;
