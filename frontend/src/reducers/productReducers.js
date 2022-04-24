6fed3103f4f844c17797fabd778a3e18b5c47354
chirazm <chirazmeddeb2@gmail.com>
2022-04-24 17:04:01 +0100

    SECOND

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,7 @@
 import { 
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
     PRODUCT_LIST_FAIL, 
     PRODUCT_LIST_RESQUEST, 
     PRODUCT_LIST_SUCCESS } 
@@ -15,4 +18,20 @@ export const productListReducer = (state = {products: []}, action)=>{
         default: 
             return state;
     }
-}
+};
+
+export const productDetailsReducer = (
+    state= { product:{}, loading: true},
+    action
+    ) => {
+        switch(action.type){
+            case PRODUCT_DETAILS_REQUEST :
+                return {loading:true};
+            case PRODUCT_DETAILS_SUCCESS : 
+                return {loading: false, product: action.payload};
+            case PRODUCT_DETAILS_FAIL : 
+                return {loading: false, error: action.payload};
+            default :
+                return state;
+        }
+    };
