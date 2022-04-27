a4ad6ffc167ef44e89574b3e77a137b2041cde43
chirazm <chirazmeddeb2@gmail.com>
2022-04-27 23:07:28 +0100

    ok

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from "bcryptjs";
 const data = {
+    users: [
+        {
+            name: 'Admin',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('12345', 8),
+            isAdmin: true,
+        },
+        {
+            name: 'User',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false,
+        },
+    ],
     products : [
         {
             _id: '1',
